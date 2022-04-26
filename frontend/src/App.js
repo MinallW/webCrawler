e5775d5f5ed51af8d575bfa89c21f026dc8610ce
MinallW <suarezmiguelc23@gmail.com>
2022-04-26 23:54:34 +0200

    Backend and Frontend Dummies

---
+++ frontend/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+
+function App() {
+  const [articleTitle, setArticleTitle] = useState("") 
+  const [articleCommentsNumber, setArticleCommentsNumber] = useState(0)
+  const [articleOrderNumber, srticleOrderNumber] = useState(0)
+  const [articlePoints, setArticlePoints] = useState(0)
+  const [dataFetched, setDataFetched] = useState(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const {data: response} = await axios.get('/api/');
+
+      } catch (error) {
+        console.error(error.message)
+      }
+    }
+  }, [])
+
+  return (
+    <>
+      <h1>Web Crawler Application</h1>
+      <Button variant="outline-primary">Primary</Button>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+      crossorigin="anonymous"
+    />
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
