1c38479061b71b4d9471ab41a4d3fa1f9db2b526
MinallW <suarezmiguelc23@gmail.com>
2022-04-27 10:28:27 +0200

    Fixing backend and frontend routes

---
+++ setup.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `articles_news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_title` varchar(50) NOT NULL,
+  `article_comment_number` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9;
