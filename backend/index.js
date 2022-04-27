f0d13bc096ff845c8e956dddea4e11b2c82493e0
MinallW <suarezmiguelc23@gmail.com>
2022-04-27 12:31:19 +0200

    Improve readability and delete unused mysql dependency

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,27 @@
 const express = require('express'),
-    mysql = require('mysql2'),
-    cors = require('cors')
+    cors = require('cors'),
+    fetch = require('node-fetch'),
+    cheerio = require('cheerio')
 
 const app = express();
-
-const fetch = require('node-fetch')
-const cheerio = require('cheerio')
-
-const getPagesArray = (numberOfPosts) =>
-  Array(Math.ceil(numberOfPosts / 30))   //divides by 30 (posts per page)
-    .fill()                          //creates a new array
-    .map((_, index) => index + 1)   //[1, 2, 3, 4,..] PagesArray
-const getPageHTML = (pageNumber) =>
-  fetch(`https://news.ycombinator.com/news?p=${pageNumber}`)
-    .then(resp => resp.text())   //Promise
-const getAllHTML = async (numberOfPosts) => {
-  return Promise.all(getPagesArray(numberOfPosts).map(getPageHTML))
-    .then(htmls => getPosts(htmls.join(''), numberOfPosts)) //one JOINED html
-}
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/crawl', (req, res) => {
-  getAllHTML(300) 
-});
+const getPagesArray = (numberOfPosts) =>
+  Array(Math.ceil(numberOfPosts / 30)) 
+    .fill()                          
+    .map((_, index) => index + 1) 
+
+const getPageHTML = (pageNumber) =>
+  fetch(`https://news.ycombinator.com/news?p=${pageNumber}`)
+    .then(resp => resp.text()) 
+
+const getAllHTML = async (numberOfPosts) => {
+  return Promise.all(getPagesArray(numberOfPosts).map(getPageHTML))
+    .then(htmls => getPosts(htmls.join(''), numberOfPosts))
+}
 
 const getPosts = (html, posts) => {
   let results = []
@@ -57,19 +53,8 @@ let obj = {
   }
 }
 
-app.get('/get', (req, res) => {
-    const SelectQuery = " SELECT * FROM articles_news";
-})
-
-app.post("/insert", (req, res) => {
-    const bookName = req.body.setBookName;
-    const bookReview = req.body.setReview;
-    const InsertQuery = "INSERT INTO books_reviews (book_name, book_review) VALUES (?, ?)";
-})
-
-app.delete("/delete/:bookId", (req, res) => {
-    const bookId = req.params.bookId;
-    const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?";
-})
+app.get('/crawl', (req, res) => {
+  getAllHTML(300) 
+});
 
 app.listen('3001', () => { console.log("API is now running...") })
